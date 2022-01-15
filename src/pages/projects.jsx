84650d8c0ef20064da468df7c5efb10d849c53ba
Wojciech Kubiak <wojciechkubiak@Wojciechs-Mac-mini.local>
2022-01-15 19:41:34 +0100

    Gatsby removal

---
+++ src/pages/projects.jsx
@@ -0,0 +1,17 @@
+import * as React from "react"
+import styled from "styled-components"
+
+const Header = styled.h1`
+  font-size: 48px;
+  color: red;
+`
+
+const ProjectsPage = () => {
+    return (
+        <main>
+            <Header>Test2</Header>
+        </main>
+    )
+}
+
+export default ProjectsPage
