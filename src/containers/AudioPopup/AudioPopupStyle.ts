0610ec22502b4fd93bcaa97b5e6e7cfaf561ecbe
wojciechkubiak <wgkubiak@gmail.com>
2022-01-28 00:39:50 +0100

    Scale fix

--- src/containers/AudioPopup/AudioPopupStyle.ts
+++ src/containers/AudioPopup/AudioPopupStyle.ts
@@ -22,6 +22,7 @@ export const AudioContainer = styled.div<IAudioContainer>`
   position: absolute;
   left: 50%;
   top: 50%;
+  transform: translate(-50%, -50%);
   z-index: 2000;
   box-shadow: rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset,
     rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
